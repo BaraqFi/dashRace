fb9915b3ad1762baa591658037c98bf372fef7e7
Moh Mubarak <Mbarrack01@gmail.com>
2024-07-19 09:11:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -57,6 +57,7 @@ function create() {
     currentLaneIndex = 1; // Start in the center lane
     car = this.physics.add.sprite(lanes[currentLaneIndex], this.cameras.main.height - 100, 'car');
     car.setCollideWorldBounds(true);
+    car.setScale(2.2); // Change the size of the car
     car.angle = -90; // Rotate car to face upwards
 
     coins = this.physics.add.group({
